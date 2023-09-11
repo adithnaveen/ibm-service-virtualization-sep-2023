b1632b128c40dee5ced72e1d68546e6f85453a37
Naveen <adith.naveen@gmail.com>
2023-09-11 17:03:09 +0530

    junit work day 1

--- workspace/java8-testing-works/src/test/java/com/ibm/businesslogic/test/SampleTestCodeTest.java
+++ workspace/java8-testing-works/src/test/java/com/ibm/businesslogic/test/SampleTestCodeTest.java
@@ -8,40 +8,61 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.ibm.businesslogic.ISampleTestingCode;
+import com.ibm.businesslogic.SampleTestCode;
+
 class SampleTestCodeTest {
 
-	
 	@BeforeAll
 	static void initiatlize() {
 		System.out.println("This is before any initialization");
 	}
-	
+
 	@AfterAll
 	static void tearDownLast() {
 		System.out.println("This is the last method to be called.. ");
 	}
-	
-	
+
 	@BeforeEach
 	public void setUp() {
 		System.out.println("Before Each Method");
 	}
-	
+
 	@AfterEach
 	public void tearDown() {
 		System.out.println("After each method ");
 	}
-	
+
 	@Test
 	void simpleTest() {
 		System.out.println("Test case ");
 		assertEquals(true, true);
 	}
-	
+
 	@Test
 	void simpleTest1() {
-		System.out.println("Test case ");
+		System.out.println("Test case1 ");
 		assertEquals(true, true);
+	}
+
+	@Test
+	void sayHelloTest_validCase() {
+		ISampleTestingCode stc = new SampleTestCode();
+
+		String name = "Souvik";
+		String returnValue = stc.sayHello(name);
+
+		assertEquals("Hello " + name, returnValue, "The expected name is wrong");
+	}
+
+	@Test
+	void sayHelloTest_invaliCase() {
+		ISampleTestingCode stc = new SampleTestCode();
+
+		String name = "Souvik";
+		String returnValue = stc.sayHello(name);
+
+		assertEquals("Hi " + name, returnValue, "The expected name is wrong");
 	}
 
 }
